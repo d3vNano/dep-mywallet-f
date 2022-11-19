4ec955ffce069ebcaebae7bf5c706746470fa4fc
dorigoluiz <luizdorigo01@gmail.com>
2022-11-19 15:31:45 -0300

    feat: SignUp conected at 99%

--- src/components/screens/SignUp.screen.js
+++ src/components/screens/SignUp.screen.js
@@ -1,20 +1,103 @@
 import styled from "styled-components";
-import { Link } from "react-router-dom";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import BeatLoader from "react-spinners/BeatLoader";
 
 function SignUpScreen() {
+    const navigate = useNavigate();
+
+    const [newUser, setNewUser] = useState({
+        name: "",
+        email: "",
+        password: "",
+    });
+
+    const [disabled, setDisabled] = useState(false);
+
+    function clearInputs() {
+        setNewUser({
+            name: "",
+            email: "",
+            password: "",
+        });
+    }
+
+    function submitForm(e) {
+        e.preventDefault();
+
+        setDisabled(true);
+
+        axios
+            .post("http://localhost:5000/sign-up", newUser)
+            .then((ans) => {
+                alert("Cadastro concluído! Agora faça o login.");
+                navigate("/");
+            })
+            .catch((err) => {
+                alert("Fala no cadastro! Tente novamente");
+                clearInputs();
+                setDisabled(false);
+            });
+    }
+
     return (
         <Screen>
             <Title>MyWallet</Title>
-            <Form>
-                <Input name="nome" type="text" placeholder="Nome" />
-                <Input name="email" type="email" placeholder="E-mail" />
-                <Input name="password" type="password" placeholder="Senha" />
+            <Form onSubmit={submitForm}>
                 <Input
+                    name="nome"
+                    type="text"
+                    placeholder="Nome"
+                    value={newUser.name}
+                    onChange={(e) =>
+                        setNewUser({ ...newUser, name: e.target.value })
+                    }
+                    disabled={disabled}
+                    required
+                />
+                <Input
+                    name="email"
+                    type="email"
+                    placeholder="E-mail"
+                    value={newUser.email}
+                    onChange={(e) =>
+                        setNewUser({ ...newUser, email: e.target.value })
+                    }
+                    disabled={disabled}
+                    required
+                />
+                <Input
+                    name="password"
+                    type="password"
+                    placeholder="Senha"
+                    value={newUser.password}
+                    onChange={(e) =>
+                        setNewUser({
+                            ...newUser,
+                            password: e.target.value,
+                        })
+                    }
+                    disabled={disabled}
+                    required
+                />
+                {/* <Input
                     name="passwordConfirm"
                     type="password"
                     placeholder="Confirme a senha"
-                />
-                <Button>Cadastrar</Button>
+                    value={newUser.passwordConfirm}
+                    onChange={(e) =>
+                        setNewUser({
+                            ...newUser,
+                            passwordConfirm: e.target.value,
+                        })
+                    }
+                    disabled={disabled}
+                    required
+                /> */}
+                <Button disabled={disabled}>
+                    {disabled ? <BeatLoader color="#FFFFFF" /> : "Cadastrar"}
+                </Button>
             </Form>
             <Link to="/">
                 <SignIn>
